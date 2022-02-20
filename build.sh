21da3987bac64a603ad96cbc3631db29dedf3bc6
Sander Poks <sanderpoks@gmail.com>
2022-02-20 23:35:59 +0200

    Lisatud windowsi build

--- build.sh
+++ build.sh
@@ -1,4 +1,23 @@
 #/bin/bash
+rm -rf __pycache__
+rm -rf dist/ build/
+rm -rf dist_linux dist_windows
+rm -f ehl_assistant.zip
+
+# Linux version
 pyinstaller -F ./ehl_assistant.py
-cp -fR chromedriver dist/
+mkdir dist/chromedriver
+cp chromedriver/chromedriver_linux dist/chromedriver/
 cp -f highlights dist/
+mv dist dist_linux
+rm -rf build/
+
+# Windows version
+wine pyinstaller -F --hidden-import redcap ./ehl_assistant.py
+mkdir dist/chromedriver
+cp chromedriver/chromedriver_windows.exe dist/chromedriver/
+cp -f highlights dist/
+mv dist ehl_assistant
+zip -r ehl_assistant.zip ehl_assistant/
+mv ehl_assistant dist_windows
+rm -rf build/
