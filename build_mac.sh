456789ffb05f9993b457c0ea204e614d88156ff6
Sander Poks <sanderpoks@gmail.com>
2022-03-06 21:52:24 +0200

    Lisatud Mac build script ja Maci zip

---
+++ build_mac.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+rm -rf __pycache__
+rm -rf dist/ build/
+rm -rf dist_mac/
+
+# Mac version
+pyinstaller -F ./ehl_assistant.py
+mkdir dist/chromedriver
+cp chromedriver/chromedriver_mac dist/chromedriver/
+cp -f highlights dist/
+cp -f version dist/
+mv dist dist_mac
+rm -rf build/
+
+mv dist_mac ehl_assistant
+version=$(head -n 1 version)
+zip -r ehl_assistant_mac_$version.zip ehl_assistant/
+mv ehl_assistant dist_mac
